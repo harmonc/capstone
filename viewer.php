25f686580be29e23fc14d76352a4d9ff3ac0a4fc
Caleb Harmon <calebmharmon@gmail.com>
2021-04-24 20:29:00 -0500

    Changed id to email

--- viewer.php
+++ viewer.php
@@ -18,7 +18,7 @@ require_once "config.php";
     if ($result->num_rows > 0) {
       // output data of each row
       while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+        echo "email: " . $row["email"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
       }
     } else {
       echo "0 results";
