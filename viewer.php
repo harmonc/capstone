05798472998f51e254621ab8bd1033408b3b2e52
Caleb Harmon <calebmharmon@gmail.com>
2021-04-24 20:24:26 -0500

    Added viewer

--- viewer.php
+++ viewer.php
@@ -1,3 +1,4 @@
+<?php
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -9,5 +10,20 @@
   <body>
     <a href="index.html" class="back">Back</a>
     <h1>Hours</h1>
+    <?php
+    $sql = "SELECT firstname, lastname, email FROM Names";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+      // output data of each row
+      while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+      }
+    } else {
+      echo "0 results";
+    }
+    $conn->close();
+
   </body>
 </html>
+?>
