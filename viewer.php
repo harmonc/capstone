f9bccfbd8942df9af755c55621af53b74cb57e99
Caleb Harmon <calebmharmon@gmail.com>
2021-04-24 20:35:37 -0500

    debug

--- viewer.php
+++ viewer.php
@@ -1,30 +1,16 @@
 <?php
 require_once "config.php";
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <meta charset="utf-8"/>
-    <title>Hours</title>
-  </head>
 
-  <body>
-    <a href="index.html" class="back">Back</a>
-    <h1>Hours</h1>
-    print_r("test")
-    $sql = "SELECT firstname, lastname, email FROM Names";
-    $result = $conn->query($sql);
+$sql = "SELECT firstname, lastname, email FROM Names";
+$result = $conn->query($sql);
 
-    if ($result->num_rows > 0) {
-      // output data of each row
-      while($row = $result->fetch_assoc()) {
-        echo "email: " . $row["email"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
-      }
-    } else {
-      echo "0 results";
-    }
-    $conn->close();
-
-  </body>
-</html>
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "email: " . $row["email"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
 ?>
