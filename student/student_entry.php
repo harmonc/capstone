fa1ba33d80935f74fa51027df5712c948eaef32e
Caleb Harmon <calebmharmon@gmail.com>
2021-04-25 20:03:19 -0500

    debug

--- student/student_entry.php
+++ student/student_entry.php
@@ -2,8 +2,8 @@
 require_once "config.php";
 
 $sql = "INSERT INTO student_hours (FName, LName, StudentID)
-VALUES ('".$_GET["fname"]."','".$_GET["lname"]."','".$_GET["studentID"]."')";
-echo $sql;
+VALUES ('".$_GET["fname"]."','".$_GET["lname"]."','".intval($_GET["studentID"])."')";
+printf($sql);
 if ($conn->query($sql) === TRUE) {
   echo "New record created successfully";
 } else {
