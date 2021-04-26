13c0d5c98577e345c64f51ef283e58851cc40264
Caleb Harmon <calebmharmon@gmail.com>
2021-04-25 20:36:56 -0500

    removed '

--- student_entry.php
+++ student_entry.php
@@ -2,7 +2,7 @@
 require_once "config.php";
 
 $sql = "INSERT INTO student_hours (FName, LName, StudentID, TotalHours, classification)
-VALUES ('".$_GET["fname"]."','".$_GET["lname"]."',".$_GET["studentID"].",".$_GET["hours"].",'".$_GET["class"]."'')";
+VALUES ('".$_GET["fname"]."','".$_GET["lname"]."',".$_GET["studentID"].",".$_GET["hours"].",'".$_GET["class"]."')";
 
 if ($conn->query($sql) === TRUE) {
   echo "New record created successfully";
