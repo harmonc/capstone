55888ae36103020323ab8b41ae4adca9b1383c72
Caleb Harmon <calebmharmon@gmail.com>
2021-04-25 20:09:28 -0500

    convert to int

--- student/student_entry.php
+++ student/student_entry.php
@@ -2,8 +2,7 @@
 require_once "config.php";
 
 $sql = "INSERT INTO student_hours (FName, LName, StudentID)
-VALUES ('".$_GET["fname"]."','".$_GET["lname"]."','".$_GET["studentID"]."')";
-console.log($sql);
+VALUES ('".$_GET["fname"]."','".$_GET["lname"]."','".intval($_GET["studentID"])."')";
 if ($conn->query($sql) === TRUE) {
   echo "New record created successfully";
 } else {
